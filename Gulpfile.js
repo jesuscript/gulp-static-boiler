c404dec690f5e4eafeff2e7b84abc10a89ba72e5
KKudryavtsev <konstantin.k.ed@gmail.com>
2016-01-21 13:24:34 +0000

    css; fixed gulp ordering

--- Gulpfile.js
+++ Gulpfile.js
@@ -3,19 +3,27 @@ var gulp = require("gulp"),
     plugins = require("gulp-load-plugins")();
 
 gulp.task("clean",function(cb){
-  rimraf("./dist", cb);
+  return gulp.src("./dist/*",{read: false})
+    .pipe(plugins.rimraf());
 });
 
-gulp.task("build-html", function(){
-  gulp.src(['./views/*.jade', "!/views/layouts/*"])
+
+gulp.task("build-css", ["clean"], function(){
+  return gulp.src("./css/*.css")
+    .pipe(gulp.dest("./dist/css/"));
+});
+
+gulp.task("build-html",["build-css"], function(){
+  return gulp.src(['./views/*.jade', "!/views/layouts/*"])
     .pipe(plugins.jade())
     .pipe(gulp.dest('./dist/'));
 });
+
 
 gulp.task("production", function(){
   
 });
 
-gulp.task("default", ["clean","build-html"] ,function(){
+gulp.task("default", ["clean","build-css","build-html"] ,function(){
   
 });
